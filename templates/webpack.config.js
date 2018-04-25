fa3a64b819d4a39598a5d88b53beda8d8fe2a752
piscium2010 <32027562+piscium2010@users.noreply.github.com>
2018-04-25 21:46:59 +0800

    Update webpack.config.js

--- templates/webpack.config.js
+++ templates/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
       extensions: ['.js', '.jsx']
     },
     module : {
-        loaders : [
+        rules : [
           {
             test : /\.jsx?/,
             exclude: /node_modules/,
